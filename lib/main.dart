83a2977c9a1480d642ce6765cd3ea4e797af5e65
zezo saad <zezosaad5000@gmail.com>
2021-05-25 11:42:21 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social/shared/components/constant.dart';
@@ -9,13 +10,32 @@ import 'layout/cubit/social_cubit.dart';
 import 'layout/social_layout_screen.dart';
 import 'modules/login/login_screen.dart';
 import 'modules/register/cubit/login_cubit.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
 
+  print("Handling a background message: ${message.messageId}");
+}
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = MyBlocObserver();
   await CacheHelper.init();
+  var token = await FirebaseMessaging.instance.getToken();
+  print(token);
+  
+  FirebaseMessaging.onMessage.listen((event) { 
+    print(event.data.toString());
+  });
 
+
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print(event.data.toString());
+  });
+  
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   Widget widget;
 
   uId = CacheHelper.getData(key: "uId");
